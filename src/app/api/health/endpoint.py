326f44b981c7cccf49b293f872bcb9c45ecf2863
Aleksandr Chugainov <achugaynov@gmail.com>
2025-03-16 13:06:24 +0200

    Явно добавил в ответ {"status": "OK"}

--- src/app/api/health/endpoint.py
+++ src/app/api/health/endpoint.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter, Response, status
 router = APIRouter()
 
 @router.get("/health", status_code=status.HTTP_200_OK)
-def health() -> Response:
-    return Response()
+def health():
+    return {"status": "OK"}
